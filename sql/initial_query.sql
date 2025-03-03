7332837b65f6205706d1cd81ead1021e52a3a1fc
Xaviar Avalos <xaviaravalos31@gmail.com>
2025-03-03 05:08:07 -0500

    Basic data exploration within the initial SQL query

--- sql/initial_query.sql
+++ sql/initial_query.sql
@@ -1,13 +1,14 @@
 -- initial_query.sql
+-- Data Exploration
+
 -- view basic information from the dataset
-SELECT
-'Transaction ID',
-'Date',
-'Customer ID',
-'Gender',
-'Age',
-'Product Category',
-'Quantity',
-'Price per Unit',
-'Total Amount'
-FROM `retail-sales-analysis`.retail_sales_dataset;
+SELECT * FROM `retail-sales-analysis`.retail_sales_dataset;
+
+-- check distinct product categories
+SELECT DISTINCT 'Product Category' FROM `retail-sales-analysis`.retail_sales_dataset;
+
+-- count total transactions
+SELECT COUNT(*) AS total_transactions FROM `retail-sales-analysis`.retail_sales_dataset;
+
+-- total revenue
+SELECT SUM(COALESCE(`Total Amount`, 0)) AS total_revenue FROM `retail-sales-analysis`.retail_sales_dataset;
